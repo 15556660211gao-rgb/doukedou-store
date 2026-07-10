e8fe99ee3998a2f6fb03b377a147b41e47786969
15556660211gao-rgb <15556660211gao@gmail.com>
2026-07-10 08:36:23 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='dkd-store-v3-logo-beads-20260710';
-const ASSETS=['/','/index.html','/style.css','/app.js','/manifest.json','/logo.png'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('/index.html'))))});
